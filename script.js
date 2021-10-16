3baf95f3e0b8d24f8b5c4b5b3f87c223bf219381
akalampv <dev.akalampv@gmail.com>
2021-10-16 12:13:09 +0300

    Generate random users

---
+++ script.js
@@ -0,0 +1,32 @@
+const main = document.querySelector("#main");
+const addUserBtn = document.querySelector("#add-user");
+const doubleBtn = document.querySelector("#double");
+const showMillionairesBtn = document.querySelector("#show-millionaires");
+const sortBtn = document.querySelector("#sort");
+const calculateWealthBtn = document.querySelector("#calculate-wealth");
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch random user and add money
+async function getRandomUser() {
+    const res = await fetch("https://randomuser.me/api");
+    const data = await res.json();
+
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    };
+
+    addData(newUser);
+}
+
+// Add new obj to data array
+function addData(obj) {
+    data.push(obj);
+}
