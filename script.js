2d0d0c5e58b69fed5ac10513077c31c2085550a5
akalampv <dev.akalampv@gmail.com>
2021-10-16 12:28:07 +0300

    Output users

--- script.js
+++ script.js
@@ -7,10 +7,6 @@ const calculateWealthBtn = document.querySelector("#calculate-wealth");
 
 let data = [];
 
-getRandomUser();
-getRandomUser();
-getRandomUser();
-
 // Fetch random user and add money
 async function getRandomUser() {
     const res = await fetch("https://randomuser.me/api");
@@ -29,4 +25,27 @@ async function getRandomUser() {
 // Add new obj to data array
 function addData(obj) {
     data.push(obj);
+
+    updateDOM();
 }
+
+// Update DOM
+function updateDOM(providedData = data) {
+    // Clear the main div
+    main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+    providedData.forEach(item => {
+        const element = document.createElement("div");
+        element.classList.add("person");
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+        main.appendChild(element);
+    });
+}
+
+// Format number as money
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event listeners
+addUserBtn.addEventListener("click", getRandomUser);
